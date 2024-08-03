cba238efe1c4958ffe2d22d26392f7920ab3f93b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-08-03 10:47:19 +0300

    Create 0001-pydantic2.py

---
+++ 0001-pydantic2.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel 
+from typing import List 
+
+class Director(BaseModel):
+    name: str
+    id: int
+    divisions: List[str]
+    
+# director_mustafa = Director(
+#     name = "Mustafa Büyükdereli",
+#     id = 101,
+#     divisions = ["FinTech", "Finance", "Accounting"]
+# )
+
+director_mustafa = {
+    "name" : "Mustafa Büyükdereli",
+    "id": 101,
+    "divisions": ["FinTech", "Finance", "Accounting"]
+}
+
+director1 = Director(**director_mustafa)
+
+print(director1) #name='Mustafa Büyükdereli' id=101 divisions=['FinTech', 'Finance', 'Accounting']
+
